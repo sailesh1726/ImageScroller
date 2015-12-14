10c08432e3fff26c072f8676aef5e8fa65671977
sailesh1726 <sailesh.android@gmail.com>
2015-12-14 02:27:16 -0800

    Access Token retrieved

---
+++ app/src/main/java/com/sparks/techie/imagescroller/AccessTokenModel.java
@@ -0,0 +1,14 @@
+package com.sparks.techie.imagescroller;
+
+
+public class AccessTokenModel {
+    private String access_token;
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+}
--- app/src/main/java/com/sparks/techie/imagescroller/AuthWebViewClient.java
+++ app/src/main/java/com/sparks/techie/imagescroller/AuthWebViewClient.java
@@ -6,6 +6,14 @@ import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
+
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
@@ -16,54 +24,89 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
-public class AuthWebViewClient extends WebViewClient{
-private String request_token;
-    private String access_token;
-private PreferenceHelper preferenceHelper;
-private Context context;
-private FetchData fetchData;
-//public String tagURLString;
+public class AuthWebViewClient extends WebViewClient {
+    private String request_token;
+    //private String access_token;
+    private PreferenceHelper preferenceHelper;
+    private Context context;
+    private FetchData fetchData;
+    private String TAG = AuthWebViewClient.class.getSimpleName();
+    private Gson gson;
+    //public String tagURLString;
     //public String tokenURLString;
     public AuthWebViewClient(Context context) {
-        this.context=context;
-        preferenceHelper=new PreferenceHelper(context);
+        this.context = context;
+        preferenceHelper = new PreferenceHelper(context);
     }
 
 
     @Override
-    public boolean shouldOverrideUrlLoading(WebView view, String url)
-    {
-        if (url.startsWith(Constants.CALLBACKURL))
-        {
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if (url.startsWith(Constants.CALLBACKURL)) {
+            Log.e(TAG, url);
             System.out.println(url);
             String parts[] = url.split("=");
             request_token = parts[1];  //This is your request token.
             //preferenceHelper.saveToken(request_token);
-            new GetConnection().execute(request_token);
+
+            StringRequest stringRequest  = new StringRequest(Request.Method.POST,
+                    Constants.TOKENURL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    response.length();
+                    gson = new Gson();
+                    AccessTokenModel accessTokenModel=gson.fromJson(response, AccessTokenModel.class);
+                    String access_token=accessTokenModel.getAccess_token();
+                    preferenceHelper.saveToken(access_token);
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    byte[] gg = error.networkResponse.data;
+                    Log.e(TAG, error.toString());
+                }
+            }){
+
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    // the POST parameters:
+                    params.put("client_id", Constants.CLIENT_ID);
+                    params.put("client_secret", Constants.CLIENT_SECRET);
+                    params.put("grant_type", "authorization_code");
+                    params.put("redirect_uri", Constants.CALLBACKURL);
+                    params.put("code", request_token);
+                    return params;
+                }
+            };
+
+            VolleyTon.getInstance().addToRequestQueue(stringRequest);
+            //new GetConnection().execute(request_token);
 //            InstagramLoginDialog.this.dismiss();
             return true;
         }
         return false;
 
     }
-    public class GetConnection extends AsyncTask<String,Void,Void> {
+
+    public class GetConnection extends AsyncTask<String, Void, Void> {
         @Override
         protected Void doInBackground(String... params) {
-            try
-            {
-                fetchData= new FetchData();
+            try {
+                fetchData = new FetchData();
 
-                access_token = fetchData.getAccessToken(params[0]);
-
-                preferenceHelper.saveToken(access_token);
+//                access_token = fetchData.getAccessToken(params[0]);
+//
+//                preferenceHelper.saveToken(access_token);
 
                 fetchData.fetchTag(context);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return null;
